c55cea5ef489efe2fdc8d2cd7856d080e3d37904
Ayrton <oayrtonsousa@gmail.com>
2026-03-22 23:42:37 -0400

    atualização no inicio

--- src/Componentes/Portfolio/Inicio/Inicio.jsx
+++ src/Componentes/Portfolio/Inicio/Inicio.jsx
@@ -43,7 +43,7 @@ const Inicio = () => {
   return (
     <>
       {/* --- INÍCIO DA SEÇÃO HERO (O PAI) --- */}
-      <section id="inicio" className="relative flex flex-col items-center justify-start md:justify-center min-h-screen pt-32 pb-20 px-6 text-center overflow-hidden">
+      <section id="inicio" className="relative flex flex-col items-center justify-center min-h-screen max-h-[850px] pt-32 pb-20 px-6 text-center overflow-hidden">
         
         {/* 1. A MALHA (FILHO ABSOLUTO) */}
         <div className="absolute inset-0 z-0 bg-grid-pattern opacity-40 animate-[waveGrid_8s_linear_infinite]"></div>
